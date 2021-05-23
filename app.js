487dd97cd5815466d75988d2227f201c225c0c5a
lamisghanayem <ghanayemlamis@gmail.com>
2021-05-24 00:51:13 +0300

    created html,css,js

---
+++ app.js
@@ -0,0 +1,13 @@
+alert ('welcome to our clinic');
+
+let user = prompt ('Are you a patient or a visitor?');
+
+let gender = prompt ('Please specify your gender?');
+
+let age = prompt (' What is your age?');
+
+let location = prompt ('where is your location?');
+
+alert ('You are a' + user + 'you gender is' + gender + 
+'your age is' + age + 'your loction is' + location);
+
